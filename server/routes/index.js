e62dcbac66de50289b2e627c153db5d0028f7e01
A. van der Pluijm <info@urbanlink.nl>
2015-02-27 10:09:52 +0100

    do not use style for asset in url
    Signed-off-by: A. van der Pluijm <info@urbanlink.nl>

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,14 +5,14 @@ var fileCtrl = require('./../controllers/file.controller'),
 
 module.exports = function(app) {
 
-  app.route('/a/:shortid/:style/:file').get(fileCtrl.serveAsset);
+  app.route('/a/:shortid/:file').get(fileCtrl.serveAsset);
 
   app.route('/d/:shortid/:file').get(fileCtrl.serveDatafile);
 
   app.post('/upload',
     // validate if user is logged in
     auth.validateUser,
-    
+
     // add file slot in db and save
     fileCtrl.save
   );
