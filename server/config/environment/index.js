54cf2561a47cdfe4e903b0dcf1e999c922871cda
A. van der Pluijm <info@urbanlink.nl>
2015-03-05 14:55:48 +0100

    Bump to version 1.0.1
    Signed-off-by: A. van der Pluijm <info@urbanlink.nl>

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -17,7 +17,7 @@ var all = {
   env: process.env.NODE_ENV,
 
   // Version of the release
-  version: '1.0.0',
+  version: '1.0.1',
 
   // Root path of server
   root: path.normalize(__dirname + '/../../..'),
