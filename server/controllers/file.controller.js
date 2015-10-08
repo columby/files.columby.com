4360c3309d0fcdc29bf62eeddb2850a5e716c32f
A. van der Pluijm <info@urbanlink.nl>
2015-10-08 14:39:15 +0200

    file controller update

--- server/controllers/file.controller.js
+++ server/controllers/file.controller.js
@@ -1,25 +1,20 @@
-'use strict';
+'use strict'
 
-var models = require('../models'),
-    config = require('../config/config'),
-    crypto = require('crypto'),
-    moment = require('moment'),
-    knox = require('knox'),
-    gm = require('gm').subClass({imageMagick:true}),
-    path = require('path'),
-    mv = require('mv'),
-    formidable = require('formidable'),
-    fs = require('fs'),
-    pg = require('pg'),
-    copyTo = require('pg-copy-streams').to;
-
+var models = require('../models')
+var config = require('../config/config')
+var knox = require('knox')
+var gm = require('gm').subClass({imageMagick: true})
+var path = require('path')
+var mv = require('mv')
+var fs = require('fs')
+var pg = require('pg')
+var copyTo = require('pg-copy-streams').to
 
 var s3client = knox.createClient({
   key: config.aws.key,
   secret: config.aws.secret,
   bucket: config.aws.bucket
-});
-
+})
 
 // Serve a static asset
 /**
@@ -27,116 +22,118 @@ var s3client = knox.createClient({
  /image/small/image.png
  /dataset/set.csv
 **/
-exports.serve = function(req, res) {
-
-  var availableStyles = ['thumbnail', 'small', 'medium', 'large', 'xlarge'];
+exports.serve = function (req, res) {
+  var availableStyles = ['thumbnail', 'small', 'medium', 'large', 'xlarge']
   // validate style
+  var style
   if (availableStyles.indexOf(req.params.style) !== -1) {
-    var style = req.params.style;
-  } else {
-    var style;
+    style = req.params.style
   }
-
-  var params = req.params;
-  var type = params.type;
-  var filename = params.filename;
-  var filepath = req.path;
-  var s3url = '/' + config.env + '/files' + filepath;
+  var params = req.params
+  var type = params.type
+  var filename = params.filename
+  var filepath = req.path
+  var s3url = '/' + config.env + '/files' + filepath
 
   // Try to get the file
-  s3client.getFile(s3url, function(err,s3res) {
-    if (err){ return handleError(res,err); }
-    if (s3res.statusCode===403) { return sendStatus(s3.statusCode); }
+  s3client.getFile(s3url, function (err, s3res) {
+    if (err) { return handleError(res, err) }
+    if (s3res.statusCode === 403) { return res.sendStatus(s3res.statusCode).json(s3res) }
 
     if (s3res.statusCode === 200) {
-      s3res.pipe(res);
-      s3res.on('error', function(err){ return handleError(res,err); });
-    } else if((s3res.statusCode === 404) && !style) {
-      return res.status(404).json('Not found');
-    } else if ( (s3res.statusCode === 404) && (style) ) {
-      var srcUrl = '/' + config.env + '/files/' + type + '/' + filename;
-      console.log('Trying to create derivative from ' + srcUrl);
+      s3res.pipe(res)
+      s3res.on('error', function (err) { return handleError(res, err) })
+    } else if ((s3res.statusCode === 404) && !style) {
+      return res.status(404).json('Not found')
+    } else if ((s3res.statusCode === 404) && (style)) {
+      var srcUrl = '/' + config.env + '/files/' + type + '/' + filename
+      console.log('Trying to create derivative from ' + srcUrl)
       // Try to create a derivative
       // Create a temporary local file for download
-      var tmpPath = path.join(__dirname, '../tmp/' + filename);
-      console.log('Local path: ' + tmpPath);
-      var file = fs.createWriteStream(tmpPath);
-      file.on('open', function(fd){
-        console.log('file opened');
-        s3client.getFile(srcUrl, function(err, origResponse) {          
+      var tmpPath = path.join(__dirname, '../tmp/' + filename)
+      console.log('Local path: ' + tmpPath)
+      var file = fs.createWriteStream(tmpPath)
+      file.on('open', function (fd) {
+        console.log('file opened')
+        s3client.getFile(srcUrl, function (err, origResponse) {
           // Handle error or not found
-          if (err){ return handleError(res,err); }
-          if (origResponse.statusCode === 404){ return res.status(404).json('Original file not found'); }
+          if (err) {
+            return handleError(res, err)
+          }
+          if (origResponse.statusCode === 404) { return res.status(404).json('Original file not found') }
           // Handle success
           if (origResponse.statusCode === 200) {
-            console.log('Original file found, downloading');
+            console.log('Original file found, downloading')
             // save s3 file to local tmp location
-            origResponse.pipe(file);
+            origResponse.pipe(file)
             // handle save error
-            origResponse.on('error', function(err){ return handleError(res, 'Error downloading original file from S3. '); });
+            origResponse.on('error', function (err) {
+              return handleError(res, 'Error downloading original file from S3. ')
+            })
             // handle save finished
             origResponse.on('end', function(){
               console.log('S3 original file downloaded to local.')
               // Create local derivative
-              var w=1600;
-              switch(style){
-                case 'thumbnail': w=80; break;
-                case 'small': w=400; break;
-                case 'medium': w=800; break;
-                case 'large': w=1200; break;
-                case 'xlarge': w=1600; break;
-                default: return handleError(res,'Not a valid style: ' + style);
+              var w = 1600
+              switch (style) {
+                case 'thumbnail': w = 80; break
+                case 'small': w = 400; break
+                case 'medium': w = 800; break
+                case 'large': w = 1200; break
+                case 'xlarge': w = 1600; break
+                default: return handleError(res, 'Not a valid style: ' + style)
               }
 
-              var resizedFilePath = path.join(__dirname, '../tmp/resized/' + filename);
-              console.log('Creating derivative ' + style + ' from ' + tmpPath + ' at ' + resizedFilePath);
-              gm(tmpPath).resize(w)
-                .write(resizedFilePath, function(err) {
-                  // Get filesize
-                  fs.stat(resizedFilePath, function(err,stats){
-                    if (err) { return handleError(res,err); }
-                    // Send file to S3
-                    console.log('Sending derived file to S3 from: ' + resizedFilePath);
-                    console.log('to ' + s3url );
-                    s3client.putFile(resizedFilePath, s3url, function(err, uploadResponse) {
-                      if (err) { return handleError(res,err); }
-                      console.log('Upload complete, status: ' + uploadResponse.statusCode);
-                      // Serve the created and uploaded derivative
-                      if (uploadResponse.statusCode === 200) {
-                        console.log('Fetching file to stream to res: ' + s3url);
-                        s3client.getFile(s3url, function(err, s3FinalResponse){
-                          console.log('done ' + s3FinalResponse.statusCode);
-                          if (s3FinalResponse.statusCode === 200) {
-                            console.log('stream');
-                            s3FinalResponse.pipe(res);
-                            s3FinalResponse.on('error', function(err){
-                              return handleError(res, 'Error downloading original file from S3. '); });
-                            // handle save finished
-                            s3FinalResponse.on('data', function(){
-                              //console.log('data');
-                            })
-                            s3FinalResponse.on('end', function(){
-                              console.log('end');
-                            });
-                          }
-                        });
-                      }
-                    });
-                  });
-                });
-            });
+              var resizedFilePath = path.join(__dirname, '../tmp/resized/' + filename)
+              console.log('Creating derivative ' + style + ' from ' + tmpPath + ' at ' + resizedFilePath)
+              gm(tmpPath).resize(w).write(resizedFilePath, function (err) {
+                // Get filesize
+                fs.stat(resizedFilePath, function (err, stats) {
+                  if (err) {
+                    return handleError (res, err)
+                  }
+                  // Send file to S3
+                  console.log('Sending derived file to S3 from: ' + resizedFilePath)
+                  console.log('to ' + s3url)
+                  s3client.putFile(resizedFilePath, s3url, function (err, uploadResponse) {
+                    if (err) { return handleError (res,err) }
+                    console.log('Upload complete, status: ' + uploadResponse.statusCode)
+                    // Serve the created and uploaded derivative
+                    if (uploadResponse.statusCode === 200) {
+                      console.log('Fetching file to stream to res: ' + s3url)
+                      s3client.getFile(s3url, function (err, s3FinalResponse) {
+                        console.log('done ' + s3FinalResponse.statusCode)
+                        if (s3FinalResponse.statusCode === 200) {
+                          console.log('stream')
+                          s3FinalResponse.pipe(res)
+                          s3FinalResponse.on('error', function (err) {
+                            return handleError(res, 'Error downloading original file from S3. ')
+                          })
+                          // handle save finished
+                          s3FinalResponse.on('data', function () {
+                            //console.log('data');
+                          })
+                          s3FinalResponse.on('end', function () {
+                            console.log('end')
+                          })
+                        }
+                      })
+                    }
+                  })
+                })
+              })
+            })
           }
-        });
-      }).on('error', function(err){
-        return handleError(res,err);
-      });
+        })
+      }).on('error', function (err) {
+        return handleError(res, err)
+      })
     } else {
-      console.log(filepath + ' not found ');
-      return res.sendStatus(s3res.statusCode);
+      console.log(filepath + ' not found ')
+      return res.sendStatus(s3res.statusCode)
     }
-  });
+  })
 }
-
 
 /**
  * Convert a database table to a csv file for a primary source, based on a primary_id
@@ -211,21 +208,16 @@ exports.convert = function(req,res){
         done();
         return res.json({status: 'error', msg:err});
       });
-
     });
-
   }).catch(function(err){
-    console.log('err',err);
+    console.log('err', err)
   })
-
-};
-
-
+}
 
 function handleError(res,err) {
-  console.log('File controller error: ', err);
+  console.log('File controller error: ', err)
   return res.json({
     status: 'error',
     msg: err
-  });
+  })
 }
